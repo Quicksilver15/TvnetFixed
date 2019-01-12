65865391a9f1cc24320a868ff2e91c2808fbd0eb
User <andy_miller@inbox.lv>
2019-01-12 21:27:11 +0200

    Fixed code.

--- src/test/java/TvNetTest.java
+++ src/test/java/TvNetTest.java
@@ -1,20 +1,25 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
 
-public class TvNetTest {
-    private final String HOME_PAGE = "www.tvnet.lv";
-    private WebDriver browser;
 
-    private final By ARTICLE = By.xpath(".//*[@itemprop = 'url']");
+public class TvNetTest {
+    private final String HOME_PAGE = ("http://tvnet.lv");
+    private WebDriver browser;
+    private final By ARTICLE = By.xpath(".//a[@class = 'list-article__url']");
     private final By COMMENT_COUNT = By.xpath(".//span[contains(@class, 'list-article__comment')]");
-    private final By ARTICLE_PAGE_COMENT_COUNT = By.xpath(".//span[contains(@class, 'heading__count')]");
-    private final By COMMENTS_BTN = By.xpath(".//a[contains(@class, 'button--comment')]");
+    private final By ARTICLE_PAGE_COMMENT_COUNT = By.xpath(".//span[@class = 'teaser-text__comment-count']");
+    private final By COMMENTS_BTN = By.xpath(".//span[@class = 'teaser-text']");
+    private final By SHARE = By.xpath(".//div[contains(@class, 'block-title')]");
+    private final By COMMENTS_PAGE_COMMENT_COUNT = By.xpath(".//span[contains(@class, 'article-comments-heading__count')]");
+
 
     @Test
     public void commentCountTest() {
@@ -24,31 +29,39 @@ public class TvNetTest {
         browser.get(HOME_PAGE);
 
         List<WebElement> articles = browser.findElements(ARTICLE);
+
         Integer commentCount = Integer.valueOf(articles.get(3).findElement(COMMENT_COUNT).getText());
 
-        getElementById(2).click();
+        getElementById(3).click();
 
-        Integer articlePageCommentCount = Integer.valueOf(browser.findElement(ARTICLE_PAGE_COMENT_COUNT).getText());
+        WebElement dalities = browser.findElement(SHARE);
+        JavascriptExecutor js = (JavascriptExecutor) browser;
+        js.executeScript("arguments[0].scrollIntoView()", dalities);
 
-        Assertions.assertTrue(commentCount == articlePageCommentCount, "Wrong Article page Comment Count");
+        WebDriverWait wait = new WebDriverWait(browser, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(ARTICLE_PAGE_COMMENT_COUNT));
+
+        String articlePageComments = browser.findElement(ARTICLE_PAGE_COMMENT_COUNT).getText();
+        Integer articlePageCommentCount = Integer.valueOf(articlePageComments.substring(1, articlePageComments.length() -1));
+
+        Assertions.assertTrue(commentCount == articlePageCommentCount, "Wrong Article page Comments Count");
 
         browser.findElement(COMMENTS_BTN).click();
 
-        Integer commentPageCommentCount = Integer.valueOf(browser.findElement(ARTICLE_PAGE_COMENT_COUNT).getText());
+        Integer commentsPageCommentCount = Integer.valueOf(browser.findElement(COMMENTS_PAGE_COMMENT_COUNT).getText());
 
-        Assertions.assertTrue(commentCount == commentPageCommentCount, "Wrong Article page Comment Count");
+        Assertions.assertTrue(commentCount == commentsPageCommentCount, "Wrong Comments page Comments Count");
     }
 
     private WebElement getElementById(int id) {
-        List<WebElement> articles = browser.findElements(ARTICLE);
+       List<WebElement> articles = browser.findElements(ARTICLE);
 
         for (int i = 0; i < articles.size(); i++) {
             WebElement element = articles.get(i);
             if (i == id) {
-                return element;
+               return element;
             }
         }
-
         return null;
     }
 }
